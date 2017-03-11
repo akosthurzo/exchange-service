ca291e6bae8eafeb481a8d41b772abac63c8f298
akosthurzo <akos.thurzo@liferay.com>
2017-03-11 16:37:36 +0100

    Update scheduled task to display the data

--- src/main/java/com/sample/exchange/ECBServiceClient.java
+++ src/main/java/com/sample/exchange/ECBServiceClient.java
@@ -1,9 +1,18 @@
 package com.sample.exchange;
 
+import com.sample.exchange.model.ecb.CurrencyRate;
+import com.sample.exchange.model.ecb.DailyCurrencyRates;
+import com.sample.exchange.model.ecb.Envelope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * @author Akos Thurzo
@@ -13,8 +22,24 @@ public class ECBServiceClient {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @Scheduled(fixedRate = 5000)
-    public void getECBData() {
+    @Scheduled(fixedRate = 60000)
+    public void getECBData() throws JAXBException, MalformedURLException {
         log.debug("Scheduled method called!");
+
+        JAXBContext jaxbContext = JAXBContext.newInstance(Envelope.class);
+
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+        URL url = new URL("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml");
+
+        Envelope envelope = (Envelope) unmarshaller.unmarshal(url);
+
+        for (DailyCurrencyRates dailyCurrencyRates : envelope.getCurrencyRatesWrapper().getDailyCurrencyRates()) {
+            log.debug(dailyCurrencyRates.getDay());
+
+            for (CurrencyRate currencyRate : dailyCurrencyRates.getCurrencyRates()) {
+                log.debug(currencyRate.getCurrency() + "=" + currencyRate.getRate());
+            }
+        }
     }
 }
