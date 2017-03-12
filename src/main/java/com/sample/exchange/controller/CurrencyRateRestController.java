dd05bf322be1b4bc52364346b0a28ce5938b631c
akosthurzo <akos.thurzo@liferay.com>
2017-03-12 15:09:34 +0100

    Handle 404 not found

--- src/main/java/com/sample/exchange/controller/CurrencyRateRestController.java
+++ src/main/java/com/sample/exchange/controller/CurrencyRateRestController.java
@@ -4,7 +4,10 @@ import com.sample.exchange.model.CurrencyRate;
 import com.sample.exchange.repository.CurrencyRateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,20 +24,26 @@ public class CurrencyRateRestController {
     @Autowired
     private CurrencyRateRepository currencyRateRepository;
 
-    @RequestMapping("/currencyRates")
-    public Iterable<CurrencyRate> findAll(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-                                          @RequestParam(value = "date") Optional<LocalDate> date,
-                                          @RequestParam(value = "currency") Optional<String> currency) {
+    @RequestMapping(value = "/currencyRates", method = RequestMethod.GET)
+    public ResponseEntity<Iterable<CurrencyRate>> currencyRates(
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @RequestParam(value = "date") Optional<LocalDate> date,
+            @RequestParam(value = "currency") Optional<String> currency) {
+
+        Iterable<CurrencyRate> ret;
 
         if (date.isPresent() && currency.isPresent())
-            return currencyRateRepository.findByDateAndCurrency(date.get(), currency.get());
+            ret = currencyRateRepository.findByDateAndCurrency(date.get(), currency.get());
+        else if (!date.isPresent() && !currency.isPresent())
+            ret = currencyRateRepository.findAll();
+        else if (date.isPresent())
+            ret = currencyRateRepository.findByDate(date.get());
+        else
+            ret = currencyRateRepository.findByCurrency(currency.get());
 
-        if (!date.isPresent() && !currency.isPresent())
-            return currencyRateRepository.findAll();
+        if (ret.iterator().hasNext())
+            return new ResponseEntity<Iterable<CurrencyRate>>(ret, HttpStatus.OK);
 
-        if (date.isPresent())
-            return currencyRateRepository.findByDate(date.get());
-
-        return currencyRateRepository.findByCurrency(currency.get());
+        return ResponseEntity.notFound().build();
     }
 }
